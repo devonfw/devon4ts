81227f421be17fb94584fd32a3fd60b90592ccf7
Magaña Mascuñana <miguel-angel.magana-mascunana-external@capgemini.com>
2018-10-17 12:34:41 +0200

    Exported User Service as needed

--- template/src/user/user.module.ts
+++ template/src/user/user.module.ts
@@ -3,11 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './models/user.entity';
-import { AuthService } from 'shared/auth/auth.service';
-
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
