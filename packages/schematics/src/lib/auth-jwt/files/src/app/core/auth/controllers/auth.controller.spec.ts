ee31a5a7593b9ed201d39a23f97e4a0be3084a13
Dario Rodriguez Gonzalez <dario.rodriguez-gonzalez@capgemini.com>
2019-10-22 22:47:49 +0200

    Renamed devon4node CLI project. Changed auth module
    Changed the auth module in order to return the JWT token
    in the header instead of body.

--- packages/schematics/src/lib/auth-jwt/files/src/app/core/auth/controllers/auth.controller.spec.ts
+++ packages/schematics/src/lib/auth-jwt/files/src/app/core/auth/controllers/auth.controller.spec.ts
@@ -33,14 +33,34 @@ describe('Auth Controller', () => {
         username: 'user1',
         password: 'user1',
       };
-      await expect(controller.login(user)).resolves.toBe('THISISNOTAJWTTOKEN');
+
+      const sendMock = {
+        send: jest.fn(),
+      };
+      const mockResponse: any = {
+        // tslint:disable-next-line: variable-name
+        status: jest.fn().mockImplementation(_x => sendMock),
+        setHeader: jest.fn(),
+      };
+      const value = await controller.login(user, mockResponse);
+      expect(value).toBeUndefined();
+      expect(mockResponse.status).toBeCalledWith(200);
+      expect(sendMock.send).toBeCalledWith();
+      expect(mockResponse.setHeader).toBeCalledWith(
+        'Authorization',
+        'Bearer THISISNOTAJWTTOKEN',
+      );
     });
     it('should throw an error when the username or password are not correct', async () => {
       await expect(
-        controller.login({
-          username: 'user2',
-          password: 'user1',
-        }),
+        controller.login(
+          {
+            username: 'user2',
+            password: 'user1',
+          },
+          // response not needed
+          {} as any,
+        ),
       ).rejects.toThrow(UnauthorizedException);
     });
   });
