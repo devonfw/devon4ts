5d2282527b52aee494860451514f3f9a1946d437
dario-rodriguez <dario.rodriguez-gonzalez@capgemini.com>
2019-10-25 12:47:13 +0200

    Added documentation pages

--- samples/components-example/src/app/middleware/global-middleware.middleware.ts
+++ samples/components-example/src/app/middleware/global-middleware.middleware.ts
@@ -5,6 +5,5 @@ export class GlobalMiddlewareMiddleware implements NestMiddleware {
   use(_req: any, _res: any, next: () => void) {
     console.log('Im global middleware');
     next();
-    console.log('Im global middleware again');
   }
 }
