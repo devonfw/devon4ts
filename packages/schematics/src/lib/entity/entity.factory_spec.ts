495a2d6c11d1bc59ab6394a8b44782ce8e9774ec
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2020-09-08 17:50:20 +0200

    test: entity schematic

---
+++ packages/schematics/src/lib/entity/entity.factory_spec.ts
@@ -0,0 +1,43 @@
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+
+describe('Entity Factory', () => {
+  const runner: SchematicTestRunner = new SchematicTestRunner('.', path.join(process.cwd(), 'src/collection.json'));
+  it('should manage name only', () => {
+    const options: object = {
+      name: 'foo',
+    };
+    runner.runSchematicAsync('entity', options).subscribe(tree => {
+      const files: string[] = tree.files;
+      expect(files.find(filename => filename === '/src/app/model/entities/foo.entity.ts')).toBeDefined();
+    });
+  });
+  it('should manage name and path', () => {
+    const options: object = {
+      name: 'foo',
+      path: 'bar',
+    };
+    runner.runSchematicAsync('entity', options).subscribe(tree => {
+      const files: string[] = tree.files;
+      expect(files.find(filename => filename === '/bar/src/app/model/entities/foo.entity.ts')).toBeDefined();
+    });
+  });
+  it('should manage name to dasherize', () => {
+    const options: object = {
+      name: 'fooBar',
+    };
+    runner.runSchematicAsync('entity', options).subscribe(tree => {
+      const files: string[] = tree.files;
+      expect(files.find(filename => filename === '/src/app/model/entities/foo-bar.entity.ts')).toBeDefined();
+    });
+  });
+  it('should manage module/name', () => {
+    const options: object = {
+      name: 'foo/bar',
+    };
+    runner.runSchematicAsync('entity', options).subscribe(tree => {
+      const files: string[] = tree.files;
+      expect(files.find(filename => filename === '/src/app/foo/model/entities/bar.entity.ts')).toBeDefined();
+    });
+  });
+});
