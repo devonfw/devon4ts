5058408717b83444855b176ad59887534264639f
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2020-09-08 17:53:26 +0200

    test: init-typeorm  schematic

--- packages/schematics/src/lib/init-typeorm/init-typeorm.factory_spec.ts
+++ packages/schematics/src/lib/init-typeorm/init-typeorm.factory_spec.ts
@@ -1,17 +1,65 @@
-import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
-const collectionPath = path.join(__dirname, '../collection.json');
+describe('Typeorm Factory', () => {
+  const runner: SchematicTestRunner = new SchematicTestRunner('.', path.join(process.cwd(), 'src/collection.json'));
+  it('should work', () => {
+    const app: object = {
+      name: '',
+    };
+    const options: object = {
+      db: 'mysql',
+    };
+    runner.runSchematicAsync('application', app).subscribe(tree => {
+      runner.runSchematicAsync('typeorm', options, tree).subscribe(tree => {
+        const files: string[] = tree.files;
+        expect(files.find(filename => filename === '/docker-compose.yml')).toBeDefined();
+        expect(files.find(filename => filename === '/ormconfig.json')).toBeDefined();
+        expect(
+          files.find(filename => filename === '/src/app/shared/model/entities/base-entity.entity.ts'),
+        ).toBeDefined();
 
-describe('schematics', () => {
-  it('works', done => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const obsTree = runner.runSchematicAsync('schematics', {}, Tree.empty());
-
-    obsTree.subscribe(tree => {
-      expect(tree.files).toEqual([]);
-      done();
+        expect(tree.readContent('/src/app/shared/model/entities/base-entity.entity.ts')).toEqual(
+          "import { PrimaryGeneratedColumn, VersionColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';\n" +
+            "import { Exclude } from 'class-transformer';\n" +
+            '\n' +
+            'export abstract class BaseEntity {\n' +
+            "  @PrimaryGeneratedColumn('increment')\n" +
+            '  id!: number;\n' +
+            '\n' +
+            '  @VersionColumn({ default: 1 })\n' +
+            '  @Exclude({ toPlainOnly: true })\n' +
+            '  version!: number;\n' +
+            '\n' +
+            '  @CreateDateColumn()\n' +
+            '  @Exclude({ toPlainOnly: true })\n' +
+            '  createdAt!: string;\n' +
+            '\n' +
+            '  @UpdateDateColumn()\n' +
+            '  @Exclude({ toPlainOnly: true })\n' +
+            '  updatedAt!: string;\n' +
+            '}\n',
+        );
+      });
+    });
+  });
+  it('should manage path', () => {
+    const app: object = {
+      name: 'foo',
+    };
+    const options: object = {
+      db: 'mysql',
+      path: 'foo',
+    };
+    runner.runSchematicAsync('application', app).subscribe(tree => {
+      runner.runSchematicAsync('typeorm', options, tree).subscribe(tree => {
+        const files: string[] = tree.files;
+        expect(files.find(filename => filename === '/foo/docker-compose.yml')).toBeDefined();
+        expect(files.find(filename => filename === '/foo/ormconfig.json')).toBeDefined();
+        expect(
+          files.find(filename => filename === '/foo/src/app/shared/model/entities/base-entity.entity.ts'),
+        ).toBeDefined();
+      });
     });
   });
 });
