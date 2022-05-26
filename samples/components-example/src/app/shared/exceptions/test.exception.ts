eaf9001c206605cf29352e5808202228f81af053
Dario Rodriguez Gonzalez <dario.rodriguez-gonzalez@capgemini.com>
2022-05-26 11:50:31 +0200

    chore(samples): move all samples to devon4node-samples repository at devonfw-sample organization

--- samples/components-example/src/app/shared/exceptions/test.exception.ts
+++
@@ -1,8 +0,0 @@
-import { HttpException } from '@nestjs/common';
-
-export class TestException extends HttpException {
-  constructor() {
-    super('Test Error', 400);
-  }
-  message = 'Im the test exception';
-}
