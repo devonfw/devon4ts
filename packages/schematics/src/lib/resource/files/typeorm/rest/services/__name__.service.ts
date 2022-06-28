0f49b03a7b67f62182ebcaa3f7b4366a7804f0af
Dario Rodriguez Gonzalez <dario.rodriguez-gonzalez@capgemini.com>
2022-06-29 00:39:46 +0200

    fix(schematics): solve problem at resource generator when name contains dashes

--- packages/schematics/src/lib/resource/files/typeorm/rest/services/__name__.service.ts
+++ packages/schematics/src/lib/resource/files/typeorm/rest/services/__name__.service.ts
@@ -13,8 +13,8 @@ export class <%=classify(name)%>Service {
   constructor(@InjectRepository(<%=classify(nameSingular)%>) private readonly repository: Repository<<%=classify(nameSingular)%>>) {}
 
   create(create<%=classify(nameSingular)%>Dto: Create<%=classify(nameSingular)%>Dto): Promise<<%=classify(nameSingular)%>> {
-    const <%=nameSingular%> = plainToClass(<%=classify(nameSingular)%>, create<%=classify(nameSingular)%>Dto);
-    return this.repository.save(<%=nameSingular%>);
+    const <%=camelize(nameSingular)%> = plainToClass(<%=classify(nameSingular)%>, create<%=classify(nameSingular)%>Dto);
+    return this.repository.save(<%=camelize(nameSingular)%>);
   }
 
   async findAll(page?: PageRequest): Promise<<%=classify(nameSingular)%>[] | Page<<%=classify(nameSingular)%>>> {
@@ -41,13 +41,13 @@ export class <%=classify(name)%>Service {
 
   async update(id: number, update<%=classify(nameSingular)%>Dto: Update<%=classify(nameSingular)%>Dto): Promise<<%=classify(nameSingular)%>> {
     const found = await this.repository.findOne(id);
-    const <%=nameSingular%> = plainToClass(<%=classify(nameSingular)%>, { ...found, ...update<%=classify(nameSingular)%>Dto, id });
+    const <%=camelize(nameSingular)%> = plainToClass(<%=classify(nameSingular)%>, { ...found, ...update<%=classify(nameSingular)%>Dto, id });
 
     if (!found) {
       throw new EntityNotFound(<%=classify(nameSingular)%>, id);
     }
 
-    return this.repository.save(<%=nameSingular%>);
+    return this.repository.save(<%=camelize(nameSingular)%>);
   }
 
   async remove(id: number): Promise<<%=classify(nameSingular)%>> {
