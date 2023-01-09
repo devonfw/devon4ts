e361a10d4b5786fd4a2b642cd7235d83a9b57ecb
Dario Rodriguez Gonzalez <dario.rodriguez-gonzalez@capgemini.com>
2023-01-09 11:50:42 +0100

    revert(config): add again config package and update to nestjs 9

---
+++ packages/config/test/config/production.ts
@@ -0,0 +1,11 @@
+import { BaseConfig } from '../../lib/base-config';
+
+const config: BaseConfig = {
+  isDev: false,
+  port: 3000,
+  host: 'productionhost',
+  clientUrl: 'productionhost:4200',
+  defaultVersion: '1',
+};
+
+export default config;
