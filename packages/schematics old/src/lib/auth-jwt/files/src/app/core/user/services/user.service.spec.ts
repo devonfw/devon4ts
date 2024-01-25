cb91f4e67e5c0edc708c75f0238f01cf677dbd65
jorgecas <jorge.castillo-alcantara@capgemini.com>
2024-01-25 15:11:38 +0100

    build: change schematics name and added jest globally

--- packages/schematics old/src/lib/auth-jwt/files/src/app/core/user/services/user.service.spec.ts
+++ packages/schematics old/src/lib/auth-jwt/files/src/app/core/user/services/user.service.spec.ts
@@ -3,6 +3,7 @@ import { UserService } from './user.service';
 import { UserRepositoryMock } from '../../../../../test/user/user.repository.mock';
 import { plainToClass } from 'class-transformer';
 import { User } from '../model/entities/user.entity';
+import { describe, it } from 'node:test';
 
 describe('UserService', () => {
   let service: UserService;
