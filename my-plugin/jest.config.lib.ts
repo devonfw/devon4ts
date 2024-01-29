85753e38baf15b2cd9f58da9329cb11e792f2e0a
Pere Cholbi <pere.cholbi-escriva.external@capgemini.com>
2024-01-29 12:22:13 +0100

    build: common modified

---
+++ my-plugin/jest.config.lib.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'my-plugin',
+  preset: './jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/my-plugin',
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.[jt]s?(x)', '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)'],
+};
---
+++ packages/common/src/pagination/model/page-details.ts
@@ -0,0 +1,5 @@
+export class PageDetails {
+  pageNumber!: number;
+  total!: number;
+  pageSize!: number;
+}
