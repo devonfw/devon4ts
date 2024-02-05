c05bbbafea5dd8aa51a70409a15c27f834c9401a
Jose Tomas Hernandez Lopez <jose-tomas.hernandez-lopez@capgemini.com>
2024-02-05 13:17:40 +0100

    build: remove unnecessary generators

--- packages/schematics/src/generators/controller/schema.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ControllerGeneratorSchema {
-  name: string;
-  project: string;
-}
--- packages/schematics/src/generators/service/schema.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ServiceGeneratorSchema {
-  name: string;
-  project: string;
-}
